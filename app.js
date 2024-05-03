ea1fe2001898cd0ec3ff7ad25aa052d73078f8ee
TheAnswer07 <moisedia7@gmail.com>
2024-05-02 17:23:41 -0700

    Giphy app ey requested & added to js file

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log ("hello world");
+
+// S1d2mntE6kFPaQQnXHICTFvg1yzEeSVC
+
+// https://api.giphy.com/v1/gifs/search
+
+fetch('https://api.giphy.com/v1/gifs/search?api_key=S1d2mntE6kFPaQQnXHICTFvg1yzEeSVC&q=ironman')
+.then(res => res.json() )
+.then(giphies => {
+    console.log(giphies);
+})
