8ff6d066f88fe7e4ab993c6a300185b4364327e7
TheAnswer07 <moisedia7@gmail.com>
2024-05-03 11:12:39 -0700

    app created with jquery

--- app.js
+++ app.js
@@ -6,14 +6,25 @@ var giphiesContainer = document.querySelector('.container');
 
 // https://api.giphy.com/v1/gifs/search
 
-fetch('https://api.giphy.com/v1/gifs/search?api_key=S1d2mntE6kFPaQQnXHICTFvg1yzEeSVC&q=avengers')
-    .then(res => res.json())
-    .then(giphies => {
-        // console.log(giphies);
-        giphies.data.forEach(gif => {
-            // console.log(gif.images.original.url);
-            var imgTag = document.createElement('img');
-            imgTag.setAttribute('src', gif.images.original.url);
-            giphiesContainer.appendChild(imgTag);
-        })
+// fetch('https://api.giphy.com/v1/gifs/search?api_key=S1d2mntE6kFPaQQnXHICTFvg1yzEeSVC&q=avengers')
+//     .then(res => res.json())
+//     .then(giphies => {
+//         // console.log(giphies);
+//         giphies.data.forEach(gif => {
+//             // console.log(gif.images.original.url);
+//             var imgTag = document.createElement('img');
+//             imgTag.setAttribute('src', gif.images.original.url);
+//             giphiesContainer.appendChild(imgTag);
+//         })
+//     })
+
+$.ajax({
+    method: 'GET',
+    url: 'https://api.giphy.com/v1/gifs/search?api_key=S1d2mntE6kFPaQQnXHICTFvg1yzEeSVC&q=avengers'
+}).then(data => {
+    data.data.forEach(gif => {
+        var imgTag = document.createElement('img');
+        imgTag.setAttribute('src', gif.images.original.url);
+        giphiesContainer.appendChild(imgTag);
     })
+})
