419a972a29f2ac61f112483b301a83e56afa8107
TheAnswer07 <moisedia7@gmail.com>
2024-05-03 10:48:08 -0700

    Giphies successfully populated on page

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-console.log ("hello world");
+console.log("hello world");
+
+var giphiesContainer = document.querySelector('.container');
 
 // S1d2mntE6kFPaQQnXHICTFvg1yzEeSVC
 
 // https://api.giphy.com/v1/gifs/search
 
-fetch('https://api.giphy.com/v1/gifs/search?api_key=S1d2mntE6kFPaQQnXHICTFvg1yzEeSVC&q=ironman')
-.then(res => res.json() )
-.then(giphies => {
-    console.log(giphies);
-})
+fetch('https://api.giphy.com/v1/gifs/search?api_key=S1d2mntE6kFPaQQnXHICTFvg1yzEeSVC&q=avengers')
+    .then(res => res.json())
+    .then(giphies => {
+        // console.log(giphies);
+        giphies.data.forEach(gif => {
+            // console.log(gif.images.original.url);
+            var imgTag = document.createElement('img');
+            imgTag.setAttribute('src', gif.images.original.url);
+            giphiesContainer.appendChild(imgTag);
+        })
+    })
